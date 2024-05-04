5f2f82270d0441c80e84cbd8727c435a4be94a38
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-05-05 01:04:38 +0200

    Added swagger info

--- src/swaggerConfig.ts
+++ src/swaggerConfig.ts
@@ -1,47 +1,46 @@
-import swaggerJsdoc from 'swagger-jsdoc'
-import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'MyBrand',
-            version: '1.0.0',
-            description: 'MyBrand API',
-            contact: {
-                name: 'Ndahimana Bonheur',
-            }
-        },
-        servers: [
-            {
-                url: 'https://my-brand-backend-server.onrender.com/api'
-            },
-            {
-                url: 'http://localhost:3301/api/'
-            },
-        ],
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'apiKey',
-                    name: 'Authorization',
-                    scheme: 'bearer',
-                    in: 'header',
-                    value: 'Bearer YOUR_TOKEN_HERE',
-                    description: 'JWT Authorization header using the Bearer scheme. Example: "Bearer {token}"',
-                },
-            },
-        },
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "MyBrand Swagger DOC",
+      version: "1.0.0",
+      description: "MyBrand API Testing",
+      contact: {
+        name: "Ndahimana Bonheur",
+        email: "ndahimana154@gmail.com",
+        website: "https://ndahimana154.vercel.app",
+      },
     },
-    apis: ['./src/routes/*.ts'],
-}
+    servers: [
+      {
+        url: "https://my-brand-backend-server.onrender.com/api",
+      },
+      {
+        url: "http://localhost:3301/api/",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "apiKey",
+          name: "Authorization",
+          scheme: "bearer",
+          in: "header",
+          value: "YOUR_TOKEN_HERE",
+          description: 'JWT Authorization header. Example: "{token}"',
+        },
+      },
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
 
-const specs = swaggerJsdoc(options)
+const specs = swaggerJsdoc(options);
 const swaggerSetup = (app: any) => {
-    app.use('/api-docs',
-        swaggerUi.serve,
-        swaggerUi.setup(specs)
-    )
-}
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+};
 
 export default swaggerSetup;
